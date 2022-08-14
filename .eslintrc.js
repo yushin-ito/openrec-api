4f7b14dc8a45bf2eae78f552c5b9c08ad9a11cf9
Yushin Ito <yitengyouxin8@gmail.com>
2022-08-15 07:54:38 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+import sum from "..";
+
+describe("test", () => {
+  it("sum test", async () => {
+    expect(sum(1, 1)).toEqual(2);
+  });
+});
